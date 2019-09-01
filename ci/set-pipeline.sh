6c505fdbb536169cad7cf8ef5eff1823bdec5abf
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2019-09-01 10:02:02 -0700

    updated set pipeline

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # my-go-packages set-pipeline.sh
 
-fly -t ci set-pipeline -p my-go-packages -c pipeline.yml --load-vars-from ../../../.credentials.yml
+fly -t ci set-pipeline -p my-go-packages -c pipeline.yml --load-vars-from ../../../../../.credentials.yml
